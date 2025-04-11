f64cc8b46e132249e6faf5a0fd563c5b15401544
Kass-S <kassiespacek101@gmail.com>
2025-04-11 03:35:19 -0700

    created card for first job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import JobCard1CSPL from "@/components/JobCard1CSPL";
 
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-h-screen max-w-screen">
+
+      <div className="flex justify-center mt-10">
+        <JobCard1CSPL />
+      </div>      
+
       
     </div>
   );
