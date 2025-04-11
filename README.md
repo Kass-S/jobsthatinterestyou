9765e2d5a9983f004a3116ce988f9192fee0a9e1
Kass-S <kassiespacek101@gmail.com>
2025-04-11 03:54:28 -0700

    second job card filled

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Kassandra Spacek
 
-4/10/25
+4/11/25
 
 Create a website with Next.JS/TypeScript about five places that you want to work at.
+
+Reviewed by: 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import JobCard1CSPL from "@/components/JobCard1CSPL";
+import JobCard2DataVisor from "@/components/JobCard2DataVisor";
 
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <div className="max-h-screen max-w-screen">
 
       <div className="flex justify-center mt-10">
-        <JobCard1CSPL />
+        <JobCard2DataVisor />
       </div>      
 
       
